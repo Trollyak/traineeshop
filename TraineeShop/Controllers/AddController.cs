43eb27b3cfa1aeb9b9143e84d465085effeba6cf
Tigr046 <tigranadtbekyan@yandex.ru>
2019-09-07 01:22:30 +0300

    kek

---
+++ TraineeShop/Controllers/AddController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraineeShop.Controllers
+{
+    public class AddController : Controller
+    {
+        // GET: Add
+        public ActionResult AddCompany()
+        {
+            return View();
+        }
+
+        
+    }
+}
