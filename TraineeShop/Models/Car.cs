24eecbd3b141f20d36a44bfeebc908cf6587ea52
Trollyak <40471208+Trollyak@users.noreply.github.com>
2019-09-07 01:47:43 +0300

    Genius design

---
+++ TraineeShop/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraineeShop.Models
+{
+    public class Car
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal? price { get; set; }
+        public Company company { get; set; }
+        public bool isPidor { get; set; }
+        
+    }
+}
---
+++ TraineeShop/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraineeShop.Models
+{
+    public class Company
+    {
+        public Guid id { get; set; }
+        public string name;
+        public string country;
+    }
+}
