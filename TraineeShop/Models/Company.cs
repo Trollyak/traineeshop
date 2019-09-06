6910656c21ed9e0e48d3a9306cbde4bb5d4adc52
Tigr046 <tigranadtbekyan@yandex.ru>
2019-09-07 01:17:08 +0300

    Мастер страницы

---
+++ TraineeShop/Models/Company.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraineeShop.Models
+{
+    public class Company
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+    }
+}
