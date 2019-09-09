e81df370cf1f515cb02636157a23ce150c5736f7
Tigr046 <tigranadtbekyan@yandex.ru>
2019-09-09 22:42:40 +0300

    Database

---
+++ TraineeShop/DataBase/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TraineeShop.Models;
+
+namespace TraineeShop.DataBase
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=zxcasdqwe");
+        }
+
+
+    }
+}
---
+++ TraineeShop/Repository/CompanyRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TraineeShop.Models;
+using TraineeShop.DataBase;
+
+namespace TraineeShop.Repository
+{
+    public class CompanyRepository : IRepository<Company>
+    {
+        private ApplicationContext db;
+        public CompanyRepository()
+        {
+            this.db = new ApplicationContext();
+        }
+        public void Create(Company item)
+        {
+            db.Companies.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            Company company = Get(id);
+            if (company != null)
+                db.Companies.Remove(company);
+            db.SaveChanges();
+        }
+
+        public Company Get(Guid id) => db.Companies.Find(id);
+
+
+        public IEnumerable<Company> GetAll() => db.Companies;
+
+        public void Update(Company item)
+        {
+            db.Update(item);
+            db.SaveChanges();
+        }
+    }
+}
