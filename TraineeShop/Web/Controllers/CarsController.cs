87eda0736c60a81169b0401f2d2a0db0b5031786
Tigr046 <tigranadtbekyan@yandex.ru>
2019-09-14 01:40:01 +0300

    New page

---
+++ TraineeShop/Web/Controllers/CarsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TraineeShop.Models;
+using TraineeShop.Repository;
+
+namespace TraineeShop.Web.Controllers
+{
+    public class CarsController : Controller
+    {
+        IRepository<Car> CarDb = new CarRepository();
+        public IActionResult Index()
+        {
+            return View(CarDb.GetAll().ToList());
+        }
+    }
+}
